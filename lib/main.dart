f92be06d25cbc41b28a827f6a3acfe8ffe0a1375
soma matsumoto <souma67jiji@icloud.com>
2024-12-12 03:44:35 +0900

    default_screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,68 +7,59 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _message = "エラーはありません";
-  String _input = "10";
+  int _counter = 0;
 
-  void _handleErrors() {
-    int? convertedNumber;
-
-    try {
-      convertedNumber = int.parse(_input);
-      setState(() {
-        _message = "変換した数字は: $convertedNumber";
-      });
-    } on FormatException catch (error) {
-      setState(() {
-        _message = "FormatException 数字に変換できませんでした: ${error.message}";
-      });
-    } finally {
-      print("finally 変換処理が終了しました");
-    }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("エラーハンドリングの例"),
+        title: const Text('Flutter カウンターアプリ'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('状態：$_message'),
-            ElevatedButton(
-                onPressed: _handleErrors,
-                child: Text('数字を解析')
+          children: <Widget>[
+            const Text(
+              'ボタンを押した回数:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
